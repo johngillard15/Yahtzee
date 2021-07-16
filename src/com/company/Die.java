a07766e8beee2ccb1d2a8414de3e244ba749f205
johngillard15 <johngillard15@gmail.com>
2021-07-15 21:17:48 -0400

    finished
    Finished in class
    - now repeat until player loses
    - keep track of wins
    - try dice ui, use String.format to get die on same line

---
+++ src/com/company/Die.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Die {
+    public int numberOfSides;
+    public int faceValue;
+
+    public Die(){
+        numberOfSides = 6;
+    }
+
+    public Die(int numberOfSides){
+        this.numberOfSides = numberOfSides;
+    }
+
+    public void roll(){
+        setFaceValue((int)(Math.random() * numberOfSides) + 1);
+    }
+
+    public int getFaceValue(){
+        return faceValue;
+    }
+
+    public void setFaceValue(int faceValue){
+        this.faceValue = faceValue;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        Die die1 = new Die();
+        Die die2 = new Die();
+
+        int total, newTotal;
+        String guess;
+
+        die1.roll();
+        die2.roll();
+        total = die1.getFaceValue() + die2.getFaceValue();
+        System.out.println(die1.getFaceValue() + " + "  + die2.getFaceValue() + " = " + total);
+        System.out.println("Will the next number be (h)igher or (l)ower?");
+        guess = scan.next();
+
+        die1.roll();
+        die2.roll();
+        newTotal = die1.getFaceValue() + die2.getFaceValue();
+        if(total == newTotal)
+            System.out.println("same number you lose");
+
+        boolean isLarger = newTotal > total;
+        System.out.println(die1.getFaceValue() + " + "  + die2.getFaceValue() + " = " + newTotal);
+        switch(guess){
+            case "h":
+                System.out.println(isLarger ? "Number is higher you WIN" : "Number is lower you LOSE");
+                break;
+            case "l":
+                System.out.println(isLarger ? "Number is lower you WIN" : "Number is higher you LOSE");
+        }
+
+        scan.close();
     }
 }
