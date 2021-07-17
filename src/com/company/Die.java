6e96f8de9765596a1582b5ea444b25ae7dd5eb20
johngillard15 <johngillard15@gmail.com>
2021-07-17 00:44:15 -0400

    game now repeats until a loss and tracks wins

--- src/com/company/Die.java
+++ src/com/company/Die.java
@@ -1,12 +1,14 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Die {
     public int numberOfSides;
     public int faceValue;
+    public List<String> numberDieFaces = new ArrayList<>();
     public final String[] SIX_SIDED_DIE_FACES = {
             "┏━━━━━━━━━━━┓\n" +
             "┃           ┃\n" +
@@ -39,46 +41,37 @@ public class Die {
             "┃  ●     ●  ┃\n" +
             "┗━━━━━━━━━━━┛"
     };
-    // thinner boxes
-    /*public final String[] SIX_SIDED_DIE_FACES = {
-            "┏━━━━━━━━━┓\n" +
-            "┃         ┃\n" +
-            "┃    ●    ┃\n" +
-            "┃         ┃\n" +
-            "┗━━━━━━━━━┛",
-            "┏━━━━━━━━━┓\n" +
-            "┃      ●  ┃\n" +
-            "┃         ┃\n" +
-            "┃  ●      ┃\n" +
-            "┗━━━━━━━━━┛",
-            "┏━━━━━━━━━┓\n" +
-            "┃      ●  ┃\n" +
-            "┃    ●    ┃\n" +
-            "┃  ●      ┃\n" +
-            "┗━━━━━━━━━┛",
-            "┏━━━━━━━━━┓\n" +
-            "┃  ●   ●  ┃\n" +
-            "┃         ┃\n" +
-            "┃  ●   ●  ┃\n" +
-            "┗━━━━━━━━━┛",
-            "┏━━━━━━━━━┓\n" +
-            "┃  ●   ●  ┃\n" +
-            "┃    ●    ┃\n" +
-            "┃  ●   ●  ┃\n" +
-            "┗━━━━━━━━━┛",
-            "┏━━━━━━━━━┓\n" +
-            "┃  ●   ●  ┃\n" +
-            "┃  ●   ●  ┃\n" +
-            "┃  ●   ●  ┃\n" +
-            "┗━━━━━━━━━┛"
-    };*/
 
     public Die(){
         numberOfSides = 6;
+        numberDieFaces.addAll(Arrays.asList(SIX_SIDED_DIE_FACES));
     }
 
     public Die(int numberOfSides){
         this.numberOfSides = numberOfSides;
+
+        for(int i = 0; i < numberOfSides; i++){
+            if(i + 1 < 10){
+                numberDieFaces.add(
+                        String.format(
+                                "┌─────┐\n" +
+                                "│  %d  │\n" +
+                                "└─────┘",
+                                i + 1
+                        )
+                );
+            }
+            else{
+                numberDieFaces.add(
+                        String.format(
+                                "┌─────┐\n" +
+                                "│  %d │\n" +
+                                "└─────┘",
+                                i + 1
+                        )
+                );
+            }
+        }
     }
 
     public void roll(){
@@ -96,12 +89,12 @@ public class Die {
     public void showDice(List<Die> dice){
         List<Scanner> scannerList = new ArrayList<>();
         for(Die die : dice)
-            scannerList.add(new Scanner(SIX_SIDED_DIE_FACES[die.getFaceValue() - 1]));
+            scannerList.add(new Scanner(numberDieFaces.get(die.getFaceValue() - 1)));
 
         while(scannerList.get(0).hasNextLine()){
-            StringBuffer line = new StringBuffer();
+            StringBuilder line = new StringBuilder();
             for(Scanner scanner : scannerList)
-                line.append(scanner.nextLine());
+                line.append(scanner.nextLine()).append(" ");
             System.out.println(line);
         }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,65 +7,84 @@ import java.util.Scanner;
 public class Main {
 
     public static void game(){
-        // Initialize variables
-        Scanner scan = new Scanner(System.in);
-        Die die1 = new Die();
-        Die die2 = new Die();
-        int total1, total2;
-        String guess;
+        Scanner scanner = new Scanner(System.in);
+        List<Die> dice = new ArrayList<>(); // Create list of dice
 
-        // Initial roll
-        die1.roll();
-        die2.roll();
-        total1 = die1.getFaceValue() + die2.getFaceValue();
+        System.out.print("How many sides will the dice have? ");
+        int numberOfSides = scanner.nextInt();
+        System.out.print("How many dice do you want to roll? ");
+        int numOfDice = scanner.nextInt();
 
-        // Print initial roll and ask user for guess
-        System.out.println(die1.getFaceValue() + " + "  + die2.getFaceValue() + " = " + total1);
-        System.out.println("Will the next number be (h)igher or (l)ower?");
-        System.out.print("Your guess: ");
-        guess = scan.next();
-
-        // Second roll
-        die1.roll();
-        die2.roll();
-        total2 = die1.getFaceValue() + die2.getFaceValue();
-
-        // Print second roll and determine winning/losing message
-        System.out.println(die1.getFaceValue() + " + "  + die2.getFaceValue() + " = " + total2);
-        if(total1 == total2){
-            System.out.println("same number you lose lol");
+        // add numOfDice dice to list
+        if(numberOfSides == 6){
+            for(int i = 0; i < numOfDice; i++)
+                dice.add(new Die());
         }
         else{
-            boolean isLarger = total2 > total1; // true = total2 is larger
-            switch (guess){
-                case "h":
-                    System.out.println(isLarger ? "Number is higher you WIN" : "Number is lower you LOSE");
-                    break;
-                case "l":
-                    System.out.println(isLarger ? "Number is higher you LOSE" : "Number is lower you WIN");
-            }
+            for(int i = 0; i < numOfDice; i++)
+                dice.add(new Die(numberOfSides));
+
         }
 
+        int wins = 0;
+        boolean isWinner = true;
+        do{
+            // roll all dice in list
+            for (Die die : dice)
+                die.roll();
 
-        scan.close();
+            int total1 = 0;
+            for (Die die : dice)
+                total1 += die.getFaceValue();
+
+
+            // Print initial roll and ask user for guess
+            dice.get(0).showDice(dice);
+            System.out.println("Total = " + total1);
+            System.out.println("Will the next roll be (h)igher or (l)ower?");
+            System.out.print("Your guess: ");
+            String guess = scanner.next();
+
+            // Second roll
+            for (Die die : dice)
+                die.roll();
+
+            int total2 = 0;
+            for (Die die : dice) {
+                total2 += die.getFaceValue();
+            }
+
+            // Print second roll and determine winning/losing message
+            dice.get(0).showDice(dice);
+            System.out.println("Total = " + total2);
+
+            if (total1 == total2) {
+                System.out.println("same number you lose");
+            } else {
+                boolean isLarger = total2 > total1;
+                switch (guess) {
+                    case "h":
+                        System.out.println(isLarger ? "Number is higher you WIN" : "Number is lower you LOSE");
+                        isWinner = isLarger;
+                        break;
+                    case "l":
+                        System.out.println(isLarger ? "Number is higher you LOSE" : "Number is lower you WIN");
+                        isWinner = !isLarger;
+                }
+            }
+
+            if(isWinner) {
+                System.out.println("Nice one! Let's play again!");
+                wins++;
+            }
+            else
+                System.out.println("Not so nice... better luck next time!");
+            System.out.println("Total wins: " + wins + "\n");
+        }while(isWinner);
     }
 
     public static void main(String[] args) {
         // repeat game as long as player is a winner
-        //game();
-
-        List<Die> dice = new ArrayList<>(); // Create list of dice
-        int numOfDice = 2; // number of dice to roll
-
-        // add numOfDice dice to list
-        for(int i = 0; i < numOfDice; i++){
-            dice.add(new Die());
-        }
-        // roll all dice in list
-        for(Die die : dice) {
-            die.roll();
-        }
-        // show all die rolls in console
-        dice.get(0).showDice(dice);
+        game();
     }
 }
