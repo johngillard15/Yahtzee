a7790d0b6339cb5f9c8d3811700a5527283a9cd9
johngillard15 <johngillard15@gmail.com>
2021-07-25 00:56:15 -0400

    removed unused import atatements

--- src/com/company/Die.java
+++ src/com/company/Die.java
@@ -1,10 +1,5 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
 public class Die {
     public int numberOfSides;
     public int faceValue;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,5 @@
 package com.company;
 
-import com.HighLow.HighLow;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
