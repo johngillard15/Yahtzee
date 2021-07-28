3bfebaf66c0d5599e8f8b80f4ee728f1f582f035
johngillard15 <johngillard15@gmail.com>
2021-07-28 19:48:41 -0400

    added game selection to main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,31 @@ package com.company;
 import com.HighLow.HighLow;
 import com.LiarsDice.LiarsDice;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-//        HighLow.play();
+        Scanner scan = new Scanner(System.in);
 
-//        Yahtzee game = new Yahtzee();
-        LiarsDice game = new LiarsDice();
+        System.out.println("Which game do you want to play?");
+        System.out.println("1. HighLow");
+        System.out.println("2. Liar's Dice");
+        System.out.println("3. Yahtzee");
+        int choice = Integer.parseInt(scan.nextLine());
 
-        game.play();
+        switch(choice){
+            case 1:
+                HighLow.play();
+                break;
+            case 2:
+                LiarsDice liarsDice = new LiarsDice();
+                liarsDice.play();
+                break;
+            case 3:
+                Yahtzee yahtzee = new Yahtzee();
+                yahtzee.play();
+                break;
+        }
     }
 }
