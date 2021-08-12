f08f3c5e21d1d602958583fb3bd47ada14fb24b7
johngillard15 <johngillard15@gmail.com>
2021-08-12 18:00:18 -0400

    added check for invalid face values

--- src/com/LiarsDice/LiarsDice.java
+++ src/com/LiarsDice/LiarsDice.java
@@ -235,13 +235,17 @@ public class LiarsDice {
     }
 
     private boolean validateBid(){
-        boolean validBid = currentBid[1] > lastBid[1]
-                || (currentBid[1] == lastBid[1] && currentBid[0] > lastBid[0]);
+        boolean validBid = (currentBid[0] >= 1 && currentBid[0] <= 6)
+                && (currentBid[1] > lastBid[1] || (currentBid[1] == lastBid[1] && currentBid[0] > lastBid[0]));
 
         if(!validBid){
             System.out.printf("\nA bid of %d [%d]'s is not currently possible.\n", currentBid[1], currentBid[0]);
             System.out.println("The next bid must have:");
-            if(lastBid[0] < 6){
+            if(currentBid[0] <= 1 || currentBid[0] >= 6){
+                System.out.print("An actual face value... \n" +
+                        "which is ");
+            }
+            else if(lastBid[0] < 6){
                 System.out.printf("%d dice, higher than a [%d]\n", lastBid[1], lastBid[0]);
                 System.out.println("OR");
             }
