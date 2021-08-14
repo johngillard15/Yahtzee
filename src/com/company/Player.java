76cac928da9dfaea4fab2ca7566d2e379c8e3205
johngillard15 <johngillard15@gmail.com>
2021-08-14 10:59:11 -0400

    removed getPlayerCount from Player because it was not needed

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -26,32 +26,4 @@ public class Player {
     public static Player addPlayer(String name, int startingDice){
         return new Player(name, startingDice);
     }
-
-    public static int getPlayerCount(int MIN, int MAX){
-        Scanner scan = new Scanner(System.in);
-
-        int numPlayers;
-        boolean validNumber = false;
-        do{
-            System.out.print("\nHow many players will there be? ");
-            numPlayers = Integer.parseInt(scan.nextLine());
-            validNumber = numPlayers >= MIN && numPlayers <= MAX;
-
-            if(!validNumber){
-                System.out.printf("You cannot have %d player%s.\n", numPlayers, numPlayers != 1 ? "s" : "");
-                System.out.printf("Please pick a number between %d and %d.\n", MIN, MAX);
-            }
-        }while(!validNumber);
-
-        return numPlayers;
-    }
-
-    public int updateScore(){
-        int roundTotal = 0;
-        for(Die die : cup.dice)
-            roundTotal += die.faceValue;
-        score += roundTotal;
-
-        return roundTotal;
-    }
 }
